47a93ea1e8b26f3b02082de439608fdec4761d68
Augusto Uricchio <augustouricchio@gmail.com>
2021-08-06 00:05:10 -0300

    Updates

---
+++ precache-manifest.afb3132b8e1f8ade8ca73872c1f440f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3d86042d724a07ce886723ed869f029",
+    "url": "/zoom-atel/index.html"
+  },
+  {
+    "revision": "3a21902898eaa1d08167",
+    "url": "/zoom-atel/static/css/main.df70d2ac.chunk.css"
+  },
+  {
+    "revision": "19d35d2e1d9576526a9f",
+    "url": "/zoom-atel/static/js/2.bbf00d01.chunk.js"
+  },
+  {
+    "revision": "af4673fdf1daf60960639c352a2968b5",
+    "url": "/zoom-atel/static/js/2.bbf00d01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0667d52a59a2ff2d43db",
+    "url": "/zoom-atel/static/js/3.91f44726.chunk.js"
+  },
+  {
+    "revision": "3a21902898eaa1d08167",
+    "url": "/zoom-atel/static/js/main.6bf4285f.chunk.js"
+  },
+  {
+    "revision": "a91491ed587ae0d8ca7d",
+    "url": "/zoom-atel/static/js/runtime-main.9540460f.js"
+  }
+]);
