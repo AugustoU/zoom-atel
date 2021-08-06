b2e272a8d52aafd39f6b217b403b6c0f05d31252
Augusto Uricchio <augustouricchio@gmail.com>
2021-08-06 00:13:29 -0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,88 +1,20 @@
-import React from 'react';
-import { ZoomMtg } from "@zoomus/websdk";
+import React,{Component} from 'react';
+import {  Route, Switch, Router } from 'react-router-dom';
+import Zoom from './Zoom'
 
+class App  extends Component {
 
-
-ZoomMtg.setZoomJSLib('https://source.zoom.us/1.9.7/lib', '/av');
-
-ZoomMtg.preLoadWasm();
-ZoomMtg.prepareWebSDK();
-
-ZoomMtg.i18n.load('en-US');
-ZoomMtg.i18n.reload('en-US');
-
-function App() {
-
-  
-  var apiSecret = 'dXNpRJHw9Oeme5tqiNEvpBsJ8EfOh0UDBB25'
-  var apiKey = 'MVfVOSCEQwGZNKFD2slZ-w'
-  var meetingNumber = 97350326810
-  var role = 0
-  var leaveUrl = 'http://localhost:3000'
-  var userName = 'React'
-  var userEmail = ''
-  var passWord = 'cPrBW6'
-
-  const crypto = require("crypto"); 
-
-  function generateSignature(apiKey, apiSecret, meetingNumber, role) {
-
-      const timestamp = new Date().getTime() - 30000;
-      const msg = Buffer.from(apiKey + meetingNumber + timestamp + role).toString(
-        "base64"
-      );
-      const hash = crypto
-        .createHmac("sha256", apiSecret)
-        .update(msg)
-        .digest("base64");
-      const signature = Buffer.from(
-        `${apiKey}.${meetingNumber}.${timestamp}.${role}.${hash}`
-      ).toString("base64");
-  
-      return signature    
-  }
-
-  function getSignature(e) {
-      let signature = generateSignature(apiKey, apiSecret, meetingNumber, role);
-      startMeeting(signature)
-  
-  }
-
-  function startMeeting(signature) {
-    document.getElementById('zmmtg-root').style.display = 'block'
-
-    ZoomMtg.init({
-      leaveUrl: leaveUrl,
-      success: (success) => {
-        console.log(success)
-
-        ZoomMtg.join({
-          signature: signature,
-          meetingNumber: meetingNumber,
-          userName: userName,
-          apiKey: apiKey,
-          userEmail: userEmail,
-          passWord: passWord,          
-          success: (success) => {
-            console.log(success)
-          },
-          error: (error) => {
-            console.log(error)
-          }
-        })
-
-      },
-      error: (error) => {
-        console.log(error)
-      }
-    })
-  }
-
-  return (
-    <div className="App">
-      
+  render() {
+    return (
+    <div className="App">      
+    
+      <Switch>
+        <Route path="/zoom/:room" component={Zoom}></Route>
+      </Switch>
+    
     </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router createBrowserHistory>
+    <Switch>
+      <Route component={App} />
+    </Switch>
+  </Router>,
   document.getElementById('root')
 );
 
